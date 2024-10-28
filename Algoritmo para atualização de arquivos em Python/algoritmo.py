42ad84088733140a4cae0c7d303585ade217af52
andersonlobo98 <andersonluissilvalobo@gmail.com>
2024-10-28 19:17:09 -0300

    Enviando arquivos

--- Algoritmo para atualização de arquivos em Python/algoritmo.py
+++ Algoritmo para atualização de arquivos em Python/algoritmo.py
@@ -1,12 +1,22 @@
-import os
-from pathlib import Path
+#Abra o arquivo que contém a lista de permissões e Leia o conteúdo do arquivo
+remove_list = ["192.168.97.225", "192.168.158.170", "192.168.201.40", "192.168.58.57"]
 
-# Obtém o diretório atual de trabalho
-current_dir = Path(os.getcwd())
-import_file = current_dir / "allow_list.txt"
+with open("allow_list.txt", "r") as file:
+    ip_addresses = file.read()
 
-with open(import_file, "r") as file:
-    content = file.read()
-print(content)
+#Converta a string em uma lista
+ip_addresses = ip_addresses.split()
 
+#Percorra a lista de remoção
+for element in remove_list:
+
+#Remova os endereços IP que estão na lista de remoção
+    if element in ip_addresses:
+        ip_addresses.remove(element)
+
+#Atualize o arquivo com a lista revisada de endereços IP
+ip_addresses = "\n".join(ip_addresses)
+
+with open("allow_list.txt", "w") as file:
+    file.write(ip_addresses)
 
